8a94d910e2b88c15f6b75198aaa4c53c640b030b
Mogoll4 <esteman2405@gmail.com>
2024-10-15 22:32:01 -0500

    INDEX ACTUALIZADO falta realizar conexiones

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,28 @@ var swiperCategories = new Swiper(".categories__container", {
         },
       },
     });
+
+    /*=========== SWIPER PRODUCTS ============*/
+
+    /*=========== PRODUCTS TABS ============*/
+    const tabs = document.querySelectorAll('[data-target]'),
+    tabsContents = document.querySelectorAll('[content]');
+
+    tabs.forEach((tab) => {
+      tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+        tabsContents.forEach((tabsContent) => {
+          tabsContent.classList.remove('active-tab');
+        });
+
+        target.classList.add('active-tab');
+
+        tabs.forEach((tab) => {
+          tab.classList.remove('active-tab');
+        });
+
+        tab.classList.add('active-tab')
+      });
+
+
+    });
